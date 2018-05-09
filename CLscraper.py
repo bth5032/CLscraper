60f815d1887525ba976d77fb87afcdacb42e06f6
Bobak Hashemi <bth5032@gmail.com>
2018-05-09 12:41:05 -0700

    minor docs cleanup

--- CLscraper.py
+++ CLscraper.py
@@ -48,31 +48,31 @@ def getListOfIdsAndUrls():
 	new_listings = {} #dictionary which holds the unique ID for each listing and the URL.
 
 	for craigslistLinkAddress in urls: 
-			f = urllib.urlopen(craigslistLinkAddress) #Open Web Address
+		f = urllib.urlopen(craigslistLinkAddress) #Open Web Address
 
-			soup = BeautifulSoup(f.read(),"html.parser") #read in html into BS4 data structure 
+		soup = BeautifulSoup(f.read(),"html.parser") #read in html into BS4 data structure 
 
-			content = str(soup.find_all("div", class_="content")[0]) #Get the body of the search, strips away all the sidebars and stuff.
+		content = str(soup.find_all("div", class_="content")[0]) #Get the body of the search, strips away all the sidebars and stuff.
 
-			soup = BeautifulSoup(content[:content.find("<h4 class=\"ban nearby\">")],"html.parser") #Remove's the links from "nearby" towns that are far away
+		soup = BeautifulSoup(content[:content.find("<h4 class=\"ban nearby\">")],"html.parser") #Remove's the links from "nearby" towns that are far away
 
-			for listing in soup.find_all("li", {"class": "result-row"}): #For each listing, find the listings by searching for results table elements. This tag also stores a unique ID for each listing
-				#grad the unique ID and URL for the listing, CHECK_OLD_LISTINGS is set, it also checks the repost of ID against the old_listings list.
-				pid = listing.attrs["data-pid"]	
-				old_pid = pid
-				url = listing.find("a", {"class": "result-title"}).attrs["href"] #finds the link by looking for a link with results-title class and extracts the url.
-				title = listing.find("a", {"class": "result-title"}).text        #finds the listing title
-				if CHECK_OLD_LISTINGS:
-					if "data-repost-of" in listing.attrs.keys():
-						old_pid = listing.attrs["data-repost-of"]
+		for listing in soup.find_all("li", {"class": "result-row"}): #For each listing, find the listings by searching for results table elements. This tag also stores a unique ID for each listing
+			#grad the unique ID and URL for the listing, CHECK_OLD_LISTINGS is set, it also checks the repost of ID against the old_listings list.
+			pid = listing.attrs["data-pid"]	
+			old_pid = pid
+			url = listing.find("a", {"class": "result-title"}).attrs["href"] #finds the link by looking for a link with results-title class and extracts the url.
+			title = listing.find("a", {"class": "result-title"}).text        #finds the listing title
+			if CHECK_OLD_LISTINGS:
+				if "data-repost-of" in listing.attrs.keys():
+					old_pid = listing.attrs["data-repost-of"]
 
-				if (pid not in old_listings) and (old_pid not in old_listings): #check if listing is in the old list
-					new_listings[pid] = (url, title)        #listing should be returned
-					old_listings.append(pid)       #add the new pid to list of ones we've seen
-					old_listings.append(old_pid)   #add the old pid to list of ones we've seen for future proofing
+			if (pid not in old_listings) and (old_pid not in old_listings): #check if listing is in the old list
+				new_listings[pid] = (url, title)        #listing should be returned
+				old_listings.append(pid)       #add the new pid to list of ones we've seen
+				old_listings.append(old_pid)   #add the old pid to list of ones we've seen for future proofing
 
-				elif (pid not in old_listings):
-					old_listings.append(pid)       #I'm not sure if the old PID gets updated when someone reposts, or if it stays as the very first PID the listing was ever posted as. This should take care of the former case.
+			elif (pid not in old_listings):
+				old_listings.append(pid)       #I'm not sure if the old PID gets updated when someone reposts, or if it stays as the very first PID the listing was ever posted as. This should take care of the former case.
 
 	return new_listings
 
@@ -83,11 +83,11 @@ def doIteration(msg):
 		msg = constructMessage(msg, new_listings)
 		print "Found new listings, about to send email: \n%s" % msg
 
-		server = smtplib.SMTP('smtp.gmail.com:587')  
-		server.starttls()  
-		server.login(username,password)  
-		server.sendmail(fromaddr, toaddrs, msg)  
-		server.quit() 
+		#server = smtplib.SMTP('smtp.gmail.com:587')  
+		#server.starttls()  
+		#server.login(username,password)  
+		#server.sendmail(fromaddr, toaddrs, msg)  
+		#server.quit() 
 	else:
 		print "No new listings found"
 
